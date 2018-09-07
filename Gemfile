ef9e6d8603234532ca732cf444492bb83ff1dc14
mattericbrown <matt.eric.brown@gmail.com>
2018-09-07 13:35:15 -0700

    added json gem to parse api, have a working script with geocoder, time to implement it in rails

--- Gemfile
+++ Gemfile
@@ -15,13 +15,17 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
+# Use Nokogiri for web scraping
 gem 'nokogiri'
 
 gem 'pry'
-
+# Geocoder for fetching coordinates of address
 gem 'geocoder'
 
 gem 'rb-readline'
+
+# gem for parsing JSON
+gem 'json'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- geocoder_test.rb
+++ geocoder_test.rb
@@ -1,8 +1,9 @@
 require 'rubygems'
 require 'geocoder'
 require 'open-uri'
+require 'json'
 
-puts "What is your address"
+puts "What is your address? EX: 1725 Slough Avenue, Scranton, PA, 18505"
 address = gets
 
 result = Geocoder.search(address)
@@ -11,8 +12,18 @@ lat = result.first.coordinates[0]
 
 lon = result.first.coordinates[1]
 
-puts lat
+url = 'https://api.darksky.net/forecast/0a08b825121b4fa5cb88137efedb0b87/'+lat.to_s+','+lon.to_s
 
-puts lon
+buffer = open(url).read
+
+parsed = JSON.parse(buffer)
+
+timezone = parsed["timezone"]
+currently = parsed["currently"]["summary"]
+hourly = parsed["hourly"]["summary"]
+degrees = parsed["currently"]["temperature"]
+
+puts "In #{timezone}, it is currently #{currently} and #{degrees} degrees. The rest of the day will be #{hourly}"
+
 
 
