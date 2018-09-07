a48599221923052a84dafcd861d1f78c7613ed84
mattericbrown <matt.eric.brown@gmail.com>
2018-09-06 17:11:12 -0700

    removed unnecessary views and added route for post forcasts, need to send information from forecast form to weather API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'forecast/index'
+  post 'forecast/index'
 
-  resources :forecast
+  resources :forecasts
 
   root 'forecast#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
